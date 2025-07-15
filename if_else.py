b4eb66a18d4007200f10958512ef9a50dee22b71
ramdani07941 <ramdani0794812@gmail.com>
2025-07-15 10:55:54 +0700

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -3,3 +3,12 @@ if angka > 5:
   print(f"{angka} adalah nilai yang lebih dari 5")
 else:
   print(f"{angka} adalah nilai yang kurang dari 5")
+
+# program elif
+angka = 5
+if angka > 1 and angka <5:
+  print(f"{angka} adalah angka yang lebih dari 1 dan kurang dari 5")
+elif angka > 5 and angka < 10:
+  print(f"{angka} adalah angka yang kurang dari 10 dan lebih dari 5")
+else:
+  print(f"{angka} adalah angka di luar dari ketentuan")
